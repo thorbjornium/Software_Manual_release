90da2569bccc916f30efe974dc38b0da7ad32905
horbjorn <120703467+horbjorn@users.noreply.github.com>
2024-11-02 01:20:53 +0100

    first commit

---
+++ site/custom-pagewidth.js
@@ -0,0 +1,24 @@
+if (window.location.pathname.endsWith('Spotify_playlist_backup.html')) {
+    document.documentElement.style.setProperty('--content-max-width', '100%');
+}
+
+/*
+if (window.location.pathname.endsWith('desktop_shortcuts.html')) {
+    document.documentElement.style.setProperty('--content-max-width', '80%');
+}
+
+if (window.location.pathname.endsWith('alt_codes_and_symbols.html')) {
+    document.documentElement.style.setProperty('--content-max-width', '40%');
+}
+
+if (window.location.pathname.endsWith('windows_key.html')) {
+    document.documentElement.style.setProperty('--content-max-width', '60%');
+}
+
+if (window.location.pathname.endsWith('essential_shortcuts.html')) {
+    document.documentElement.style.setProperty('--content-max-width', '60%');
+}
+    */
+
+
+
