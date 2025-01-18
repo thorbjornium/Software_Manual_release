ecdf8bfe83a839d931bd7d48d7c4577911816dd8
horbjorn <120703467+horbjorn@users.noreply.github.com>
2025-01-18 23:09:48 +0100

    ..

--- site/custom-pagewidth.js
+++ site/custom-pagewidth.js
@@ -1,4 +1,4 @@
-if (window.location.pathname.endsWith('Spotify_playlist_backup')) {
+if (window.location.pathname.endsWith('Spotify_playlist_backup.html')) {
     document.documentElement.style.setProperty('--content-max-width', '100%');
 }
 
