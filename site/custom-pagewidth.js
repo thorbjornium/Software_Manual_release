9fb2f9fa47dcfa7b0516b981953e1bcca83636c4
horbjorn <120703467+horbjorn@users.noreply.github.com>
2025-01-26 16:24:00 +0100

    Spelling

--- site/custom-pagewidth.js
+++ site/custom-pagewidth.js
@@ -5,6 +5,7 @@ if (window.location.pathname.endsWith('spotify_playlist_backup')) {
 if (window.location.pathname.endsWith('desktop_shortcuts')) {
     document.documentElement.style.setProperty('--content-max-width', '100%');
 }
+
 if (window.location.pathname.endsWith('windows_key')) {
     document.documentElement.style.setProperty('--content-max-width', '100%');
 }
