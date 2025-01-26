d3c9668ec5e762fcf6f770be328232d7b30f7d41
horbjorn <120703467+horbjorn@users.noreply.github.com>
2025-01-26 15:04:37 +0100

    ..styling

--- site/custom-pagewidth.js
+++ site/custom-pagewidth.js
@@ -1,3 +1,10 @@
 if (window.location.pathname.endsWith('spotify_playlist_backup')) {
     document.documentElement.style.setProperty('--content-max-width', '100%');
 }
+
+if (window.location.pathname.endsWith('desktop_shortcuts')) {
+    document.documentElement.style.setProperty('--content-max-width', '100%');
+}
+if (window.location.pathname.endsWith('windows_key')) {
+    document.documentElement.style.setProperty('--content-max-width', '100%');
+}
