0561cb80dbfe31c01390de781b3863017e51a87f
thorbjornium <rostkrom@gmail.com>
2025-08-21 12:40:54 +0200

    Add bash test operator

--- site/enlarge-window.js
+++ site/enlarge-window.js
@@ -1,3 +1,7 @@
-if (window.location.pathname.endsWith('mikrotik_single_vlan_for_wifi.html')) {
-    document.documentElement.style.setProperty('--content-max-width', '1250px');
+// if (window.location.pathname.endsWith('mikrotik_single_vlan_for_wifi.html')) {
+//     document.documentElement.style.setProperty('--content-max-width', '1250px');
+// }
+
+if (window.location.pathname.endsWith('linux_bash_scripting.html')) {
+    document.documentElement.style.setProperty('--content-max-width', '1200px');
 }
